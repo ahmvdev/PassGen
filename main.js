812ca371990cd97e15e7444b4f813fbf1c11f437
candy <ahmadsaeed3290@gmail.com>
2024-06-24 07:49:33 +0500

    add random num char after check

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,"!", "@", "#", "$", "%" ]
+const specialChars= ["!", "@", "#", "$", "%"]
 const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const genEl = document.getElementById("generate-el")
 const inputEl = document.getElementById("input-el")
@@ -23,8 +24,8 @@ function checkSpecialChar(val) {
     }
     else {
         let value = inputEl.value;
-        let specialCharacterAdd = "$";
-        let specialNumAdd ="3";
+        let specialCharacterAdd = specialChars[Math.floor(Math.random()* specialChars.length)];
+        let specialNumAdd = nums[Math.floor(Math.random()* nums.length)];
         let position = Math.random() * value.length;
         inputEl.value = value.substring(0, position-1) + specialCharacterAdd + specialNumAdd + value.substring(position);
     }
